7d419f564c3fc17468a0465c1a13e9fd94eadb8a
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-01-28 16:49:34 +0200

    finalising authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { UserModule } from 'src/user/user.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthResolver, AuthService],
+  providers: [AuthResolver, AuthService, PrismaService, JwtService],
   imports: [UserModule, PrismaModule],
 })
 export class AuthModule {}
--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,37 +1,51 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, Context } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { Auth } from './entities/auth.entity';
 import { CreateAuthInput } from './dto/create-auth.input';
 import { UpdateAuthInput } from './dto/update-auth.input';
-import { UseFilters } from '@nestjs/common';
+import { BadRequestException, UseFilters } from '@nestjs/common';
 import { GraphQlExceptionHandler } from 'src/filter/custom-handler.filter';
+import { LoginResponse, RegisterResponse } from './types';
+import { LoginDTO, RegisterDTO } from './dto/auth.dto';
+import { Request, Response } from 'express';
 @UseFilters(GraphQlExceptionHandler)
 @Resolver(() => Auth)
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
-
-  @Mutation(() => Auth)
-  createAuth(@Args('createAuthInput') createAuthInput: CreateAuthInput) {
-    return this.authService.create(createAuthInput);
+  @Mutation(() => RegisterResponse)
+  async register(
+    @Args('registerInput') registerDto: RegisterDTO,
+    @Context() context: { res: Response },
+  ) {
+    if (registerDto.password !== registerDto.confirmPassword) {
+      throw new BadRequestException({
+        confirmPassword: 'password and confirm password are not the same',
+      });
+    }
+    const { user } = await this.authService.register(registerDto, context.res);
+    return { user };
   }
-
-  @Query(() => [Auth], { name: 'auth' })
-  findAll() {
-    return this.authService.findAll();
+  @Mutation(() => LoginResponse)
+  async login(
+    @Args('loginInput') loginDto: LoginDTO,
+    @Context() context: { res: Response },
+  ) {
+    return this.authService.login(loginDto, context.res);
   }
-
-  @Query(() => Auth, { name: 'auth' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.authService.findOne(id);
+  @Mutation(() => {
+    String;
+  })
+  async logout(@Context() context: { res: Response }) {
+    return this.authService.logout(context.res);
   }
-
-  @Mutation(() => Auth)
-  updateAuth(@Args('updateAuthInput') updateAuthInput: UpdateAuthInput) {
-    return this.authService.update(updateAuthInput.id, updateAuthInput);
-  }
-
-  @Mutation(() => Auth)
-  removeAuth(@Args('id', { type: () => Int }) id: number) {
-    return this.authService.remove(id);
+  @Mutation(() => {
+    String;
+  })
+  async refreshToken(@Context() context: { req: Request; res: Response }) {
+    try {
+      return this.authService.refreshToken(context.req, context.res);
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
   }
 }
