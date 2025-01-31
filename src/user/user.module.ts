9f9399780210d88f56357245eb00cd3834c114da
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-01-31 07:50:47 +0200

    adding the resolvers

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './services/user.service';
 import { UserResolver } from './resolvers/user.resolver';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [PrismaModule],
-  providers: [UserResolver, UserService],
+  providers: [UserResolver, UserService, JwtService],
 })
 export class UserModule {}
