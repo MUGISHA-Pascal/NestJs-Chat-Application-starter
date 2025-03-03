8aae5eafb768efd98174aa5e4f1aa1f8be5ecf25
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-03-03 13:59:30 +0200

    adding some changes

--- src/live-chatroom/resolvers/live-chatroom.resolver.ts
+++ src/live-chatroom/resolvers/live-chatroom.resolver.ts
@@ -1,35 +1,48 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
-import { LiveChatroomService } from './live-chatroom.service';
-import { LiveChatroom } from './entities/live-chatroom.entity';
-import { CreateLiveChatroomInput } from './dto/create-live-chatroom.input';
-import { UpdateLiveChatroomInput } from './dto/update-live-chatroom.input';
-
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  Int,
+  Subscription,
+  Context,
+} from '@nestjs/graphql';
+import { LiveChatroom } from '../entities/live-chatroom.entity';
+import { LiveChatroomService } from '../services/live-chatroom.service';
+import { PubSub } from 'graphql-subscriptions';
+import { UserService } from 'src/user/services/user.service';
+import { User } from 'src/user/types/user.type';
+import { UseFilters, UseGuards } from '@nestjs/common';
+import { GraphQlExceptionHandler } from 'src/filter/custom-handler.filter';
+import { GraphqlAuthGuard } from 'src/auth/graphql-auth.guard';
 @Resolver(() => LiveChatroom)
 export class LiveChatroomResolver {
-  constructor(private readonly liveChatroomService: LiveChatroomService) {}
-
-  @Mutation(() => LiveChatroom)
-  createLiveChatroom(@Args('createLiveChatroomInput') createLiveChatroomInput: CreateLiveChatroomInput) {
-    return this.liveChatroomService.create(createLiveChatroomInput);
+  private pubSub: PubSub;
+  constructor(
+    private readonly liveChatroomService: LiveChatroomService,
+    private readonly userService: UserService,
+  ) {
+    this.pubSub = new PubSub();
   }
-
-  @Query(() => [LiveChatroom], { name: 'liveChatroom' })
-  findAll() {
-    return this.liveChatroomService.findAll();
+  @Subscription(() => [User], {
+    nullable: true,
+    resolve: (value) => value.liveUsers,
+    filter: (payload, variables) => {
+      return payload.chatroomId === variables.chatroomId;
+    },
+  })
+  liveUsersInChatroom(@Args('chatroomId') chatroomId: number) {
+    return this.pubSub.asyncIterableIterator(
+      `liveUsersInChatroom.${chatroomId}`,
+    );
   }
-
-  @Query(() => LiveChatroom, { name: 'liveChatroom' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.liveChatroomService.findOne(id);
-  }
-
-  @Mutation(() => LiveChatroom)
-  updateLiveChatroom(@Args('updateLiveChatroomInput') updateLiveChatroomInput: UpdateLiveChatroomInput) {
-    return this.liveChatroomService.update(updateLiveChatroomInput.id, updateLiveChatroomInput);
-  }
-
-  @Mutation(() => LiveChatroom)
-  removeLiveChatroom(@Args('id', { type: () => Int }) id: number) {
-    return this.liveChatroomService.remove(id);
+  @UseFilters(GraphQlExceptionHandler)
+  @UseGuards(GraphqlAuthGuard)
+  @Mutation(() => boolean)
+  async enterChatroom(
+    @Args('chatroomId') chatroomId: number,
+    @Context() context: { req: Request },
+  ) {
+    const user = await this.userService.getUser;
   }
 }
